40869957188de29a40f58a13932b48e256b32922
leanhit <theanh.it@gmail.com>
2025-04-01 18:01:34 +0700

    update role

---
+++ src/main/java/com/example/demo/auth/dto/UserResponse.java
@@ -0,0 +1,13 @@
+package com.example.demo.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserResponse {
+    private Long id;
+    private String username;
+    private String email;
+    private String role;
+}
